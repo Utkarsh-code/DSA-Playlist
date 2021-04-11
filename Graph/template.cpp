48382b3b7668469db72362de93a3fbd34055015c
Utkarsh-code <utkarshvns9793@gmail.com>
2021-04-11 12:17:33 +0530

    CP template

---
+++ Graph/template.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define  int long long 
+#define  vi vector<int>
+#define  pi pair<int, int>
+#define  pii pair<int, pair<int, int>>
+
+int mod=1e9+7;
+int inf=1e18;
+int ninf=-1e18;
+
+int pown(int x, int y){
+    int res=1;
+
+    while(y){
+        if(y&1) res=(x*res)%mod;
+         x=(x*x)%mod;
+       y>>=1;
+    }
+    return res;
+}
+
+void solve(){
+    
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int tc;
+    cin>>tc;
+    for(int i=1; i<=tc; i++){
+        //cout << "Case #" << i  << ": ";
+        solve();
+    }
+    return 0;
+}
